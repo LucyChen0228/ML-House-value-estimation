ad3c17811f3950b8e121aba1bada02be87f6f69d
Lucy <42339395+LucyChen0228@users.noreply.github.com>
2018-11-23 23:28:53 +0800

    Update update 版本.py

--- update 版本.py
+++ update 版本.py
@@ -9,24 +9,21 @@ from sklearn.externals import joblib
 # Load the data set
 df = pd.read_csv("ml_house_data_set_updated.csv")
 
-# Remove the fields from the data set that we don't want to include in our model
+
+'''删去无用的变量'''
 del df['house_number']
 del df['unit_number']
 del df['street_name']
 del df['zip_code']
-'''del df 的意思是什么,删去无用的变量'''
 
-# Replace categorical data with one-hot encoded data
+
 features_df = pd.get_dummies(df, columns=['garage_type', 'city'])
 ''''pd_get_dummies 将离散特征取值之间没有大小关系的，转为one-hot 编码(机器学习中常用的二进制编码，进行二进制操作，columns指定需要实现类别转换的列名'''
 
-# Remove the sale price from the feature data
+'''因为预测的是sale_price， 故先删除'''
 del features_df['sale_price']
 
-'''问题：sale_price 并不属于feature_df 类比里面？'''
-'''如果不转化为one-hot 编码会怎么样？'''
 
-# Create the X and y arrays
 X = features_df.as_matrix()
 y = df['sale_price'].as_matrix()
 
@@ -34,6 +31,9 @@ y = df['sale_price'].as_matrix()
 #将数据集和训练集分开
 X_train, X_test, y_train ,y_test = train_test_split(X,y,test_size=0.3,random_state=0)
 
+
+
+'''gradient boosting算法'''
 model = ensemble.GradientBoostingRegressor(
     n_estimators=1000,
     learning_rate=0.1,
